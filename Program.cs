46d9b89038fc64a0eb385e4889da4d2e6cd43ed1
Dharlok <you@example.com>
2020-06-13 18:08:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+    namespace Excel2 
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                _Application excel = new _Excel.Application();
+                Workbook wb;
+                Worksheet ws;
+                int i = 1, j = 1;
+                wb = excel.Workbooks.Open(@"C:\Users\бююбт\Desktop\C#.xlsx");
+                ws = wb.Worksheets[1];
+                if (ws.Cells[i, j].Value2 != null)
+                    MessageBox.Show(ws.Cells[i, j].Value2);
+                else
+                    MessageBox.Show("");
+            }
+        }
+
+
+    }
+
